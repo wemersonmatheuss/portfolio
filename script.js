c78880ec79b257f95c0cd1f5562f9695ce896743
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-01-25 19:54:03 -0300

    add botãao de download do curriclo e colocando um alert avisando que esta sem o download direto

---
+++ script.js
@@ -0,0 +1,5 @@
+const downloadButton = document.querySelector(".button-download")
+
+function downloadCv () {
+    alert("Você deseja fazer o download do meu CV, logo mais você irá conseguir fazer o download!")
+}
