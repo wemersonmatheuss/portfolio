f5a77af462ab88c857f0d6295e19377e3f2c1e39
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-01-31 22:24:39 -0300

    gerando o download do curriculo

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const downloadButton = document.querySelector(".button-download")
 function downloadCv () {
     alert("Você deseja fazer o download do meu CV, logo mais você irá conseguir fazer o download!")
 }
+
