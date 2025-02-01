1111a4fc4b04eb2a2e0982a37bb14b341274a589
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-01-31 23:05:44 -0300

    adicionando funcionalidade no menu

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function downloadCv () {
     alert("Você deseja fazer o download do meu CV, logo mais você irá conseguir fazer o download!")
 }
 
+function toggleMenu() {
+    var menu = document.getElementById("menuOpcoes");
+    menu.classList.toggle("ativo"); // Adiciona/remove a classe "ativo"
+}
+
+// Fechar o menu ao clicar fora dele
+document.addEventListener("click", function(event) {
+    var menu = document.getElementById("menuOpcoes");
+    var menuContainer = document.querySelector(".menu-responsivo");
+
+    if (!menuContainer.contains(event.target)) {
+        menu.classList.remove("ativo");
+    }
+});
