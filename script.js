3a9d458008a6f4d4430b962c57d52c78e2f0ef1c
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-02-22 14:01:45 -0300

    ajuste no clique do menu

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const imgMenu = document.getElementById("icon-nav")
 const menuItens = document.getElementById("menu-itens")
 
 imgMenu.addEventListener("click", function() {
-    if (menuItens.style.display === "block") {
-        menuItens.style.display = "none";
-    }else {
+    if (menuItens.style.display === "none") {
         menuItens.style.display = "block";
+    }else {
+        menuItens.style.display = "none";
     }
 });
