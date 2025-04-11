6dafeb1f92119cf4911f756baf45c1d4c001e256
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-04-11 20:14:25 -0300

    adicionando as funções aos botões dos projetos e redirecionando os links do menu

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const imgMenu = document.getElementById("icon-nav")
-const menuItens = document.getElementById("menu-itens")
+// Função para abrir o site do Apito de ouro
+function openRepoProjectOne() {
+    window.open("https://www.apitodeouro.com.br/", "_blank")
+}
 
-imgMenu.addEventListener("click", function() {
-    if (menuItens.style.display === "none") {
-        menuItens.style.display = "block";
-    }else {
-        menuItens.style.display = "none";
-    }
-});
+// Função para abrir o repositorio do projeto W.M Freelancer
+function openRepoProjectTwo() {
+    window.open("https://github.com/wemersonmatheuss/wm-freelancer", "_blanck")
+}
+
+// Função para abrir o projeto W.M Freelancer
+function openWindowProjectTwo() {
+    window.open("https://wm-freelancer.vercel.app/", "_blank")
+}
+
+// Função para abir o repositorio do projeto LinkBio
+function openRepoProjectThree() {
+    window.open("https://github.com/wemersonmatheuss/link-bio", "_blank")
+}
+
+// Função para abrir o projeto LinkBio
+function openWindowProjectThree() {
+    window.open("https://link-bio-vt91.vercel.app/", "_blank")
+}
+
+// Função para abir o repositorio do projeto Sabor & Arte
+function openRepoProjectFour() {
+    window.open("https://github.com/wemersonmatheuss/restaurante-sabor-e-arte", "_blank")
+}
+
+// Função para abrir o projeto Sabor&Arte
+function openWindowProjectFour() {
+    window.open("https://restaurante-sabor-e-arte-mauve.vercel.app/", "_blank")
+}
