645662822f7bc145bbb67f65abf9239c9a817d27
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-29 21:55:19 -0300

    feat: iniciando o repositorio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {} from "./global.css"
+import styles from "./app.module.css"
+
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.css"
+
+export function Nav() {
+    <div className={styles.container}>
+        
+    </div>
+}
