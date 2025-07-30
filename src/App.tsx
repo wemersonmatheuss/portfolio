de8c90160bd7a5ed9f2d8ef0e31a601cd717f3f1
wemersonmatheuss <wemersonmthdev@gmail.com>
2025-07-29 22:20:54 -0300

    feat: criar componente de nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {} from "./global.css"
 import styles from "./app.module.css"
 
+import { Nav } from "./components/Nav"
+
 
 export function App() {
   return (
     <div className={styles.container}>
-      
+      <Nav />
     </div>
   )
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,26 @@
 import styles from "./styles.module.css"
 
+import logo from "../../assets/Logo-WemersonDev 1.svg"
+
+import { Button } from "../Button"
+
 export function Nav() {
-    <div className={styles.container}>
-        
-    </div>
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.logo}>
+                <img src={logo} alt="Logo do Wemerson" />
+            </div>
+
+            <div className={styles.navigation}>
+                <div className={styles.links}>
+                    <a href="#">Serviços</a>
+                    <a href="#">Projetos</a>
+                    <a href="#">Sobre</a>
+                </div>
+                <Button title="Contrate"/>
+            </div>
+        </div>  
+    )
+    
 }
